ed120c96f7b1222d35d56b6bc30fae07860fa456
Warnoffline <hametshin543@gmail.com>
2023-07-14 23:15:02 +0300

    new1

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <div class="app">
-    <nav-bar></nav-bar>
-    <router-view></router-view>
+    <div v-if="isLoad" class="main">
+      <nav-bar></nav-bar>
+      <router-view></router-view>
+    </div>
+    <div v-else class="loading">
+      <p>Загрузка...</p>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar.vue'
 export default{
-  components: {NavBar}
-
+  components: {NavBar},
+  data(){
+    return{
+      isLoad: false
+    }
+  },
+  mounted(){
+    window.onload = () =>{
+      this.isLoad = true
+    }
+  }
 }
 </script>
 <style lang="scss">
-#app{
-  font-family: 'Roboto Mono', monospace;
+.loading{
+  background-color: #AC011C;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 35px;
+  font-weight: bold;
 }
 </style>
